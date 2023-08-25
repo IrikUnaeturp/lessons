75f9ab14bc1f594a0e48edbd2f8fc669af804e96
IrikUnaeturp <137446829+IrikUnaeturp@users.noreply.github.com>
2023-08-25 22:52:11 +0300

    js exercise nr.4 from leetcode

--- leetcode.js
+++ leetcode.js
@@ -11,20 +11,36 @@
 // console.log(counter()); // 12
 
 //toBe or notTobe
-let expect = function(val) {
-  return{
-    toBe: (val2) =>{
-        if (val !== val2) console.log ('notEquel')
-        else console.log('true')
-    },
-    notToBe:(val2) =>{
-        if(val === val2) console.log('Equal')
-        else console.log('true')
-    }
-  }
+// let expect = function(val) {
+//   return{
+//     toBe: (val2) =>{
+//         if (val !== val2) console.log ('notEquel')
+//         else console.log('true')
+//     },
+//     notToBe:(val2) =>{
+//         if(val === val2) console.log('Equal')
+//         else console.log('true')
+//     }
+//   }
+// };
+
+
+// expect(5).toBe(5); // true
+// expect(5).notToBe(5); // "Equal"
+ 
+//counter2
+let createCounter = function(init) {
+   let  counter =  init
+   return{
+    increment:() => ++counter,
+    decrement:() => --counter,
+    reset:() => counter  = init
+   }
 };
 
 
-expect(5).toBe(5); // true
-expect(5).notToBe(5); // "Equal"
+const counter = createCounter(5)
+console.log(counter.increment()); // 6
+console.log(counter.reset()); // 5
+console.log(counter.decrement()); // 4
  
