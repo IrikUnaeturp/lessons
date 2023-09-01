e07b15c5d22cd9a8b9a5af26d4a0b70772049d03
IrikUnaeturp <137446829+IrikUnaeturp@users.noreply.github.com>
2023-09-01 22:29:56 +0300

    дошла до 10 задачи

--- arraysMethods.js
+++ arraysMethods.js
@@ -18,15 +18,105 @@
 // console.log(filtered)
 
 //задача  3
-let arr = [5, 3, 8, 1]
-function filterRangeInPlace(arr, a, b){
-    const filtered = arr.filter((e) => e >= a && e <= b)
-    while (arr.length !== 0) {
-    arr.pop()
-    }
-    for (let i = 0; i < filtered.length; i++) {
-    arr.push(filtered[i])
-    }
-    }
-console.log(filterRangeInPlace(arr,1,3))
-console.log(arr)
+// let arr = [5, 3, 8, 1]
+// function filterRangeInPlace(arr, a, b){
+//     const filtered = arr.filter((e) => e >= a && e <= b)
+//     while (arr.length !== 0) {
+//     arr.pop()
+//     }
+//     for (let i = 0; i < filtered.length; i++) {
+//     arr.push(filtered[i])
+//     }
+//     }
+// console.log(filterRangeInPlace(arr,1,3))
+// console.log(arr)
+
+//задача 4
+
+// let arr = [5, 2, 1, -10, 8];
+// function sortDecreasing(a, b){
+//  if (a>b) return 1
+//  if (b>a) return -1
+// }
+// let acc = arr.sort(sortDecreasing)
+// let answer = arr.reverse(acc)
+// console.log(answer) //это моя наркомания, но работает
+
+// let arr = [5, 2, 1, -10, 8];
+
+// arr.sort((a, b) => b - a);
+// console.log(arr)//как рекомендует автор
+
+
+//задача 5
+// let arr = ["HTML", "JavaScript", "CSS"];
+// let copy =  []
+// function copySorted(){
+//     return copy.concat(arr).sort()
+    
+// }
+
+// let sorted = copySorted(arr);
+
+// console.log(sorted)
+// console.log(arr)
+
+
+//задача 6
+
+// function Calculator(){
+//     this.methods ={
+//         "-":(a,b) => a - b,
+//         "+":(a,b) =>a + b
+//     }
+//     this.calculate=function(str){
+//         let split = str.split(' '),
+//         a = +split[0],
+//         op = split[1],
+//         b = +split[2]
+//         if (!this.methods[op] || isNaN(a) || isNaN(b)) {
+//             return NaN;
+//           }
+
+//     return this.methods[op](a, b);
+//     }
+//     this.addMethod = function(name, func) {
+//         this.methods[name] = func;
+//     };
+// }
+// let calc = new Calculator
+// console.log( calc.calculate("3 + 7") )
+
+//задача  7
+// let vasya = { name: "Вася", age: 25 };
+// let petya = { name: "Петя", age: 30 };
+// let masha = { name: "Маша", age: 28 };
+
+// let users = [ vasya, petya, masha ];
+
+// let names = users.map((item)=>item.name)
+
+// console.log( names ); // Вася, Петя, Маша
+
+//задача 8
+// let vasya = { name: "Вася", surname: "Пупкин", id: 1 };
+// let petya = { name: "Петя", surname: "Иванов", id: 2 };
+// let masha = { name: "Маша", surname: "Петрова", id: 3 };
+
+// let users = [ vasya, petya, masha ];
+// let usersMapped = users.map(user => ({
+//     fullName: `${user.name} ${user.surname}`,
+//     id: user.id
+//   }))
+// console.log( usersMapped[0].id ) // 1
+// console.log( usersMapped[0].fullName ) // Вася Пупкин
+
+//задача 9
+let arr = [1, 2, 3];
+function shuffle(){
+    return arr.sort(()=>Math.random()-0.5)
+}
+
+console.log(shuffle(arr))
+console.log(shuffle(arr))
+console.log(shuffle(arr))
