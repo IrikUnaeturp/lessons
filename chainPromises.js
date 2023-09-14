2fa38b087500005ab9dbd785cfec71ea51eaf7b2
IrikUnaeturp <137446829+IrikUnaeturp@users.noreply.github.com>
2023-09-14 22:29:31 +0300

    Дополняю задачей из темы

--- chainPromises.js
+++ chainPromises.js
@@ -1,17 +1,34 @@
-let f1 = 1
-let f2 = 2
-let promise = new Promise(function(resolve, reject) {
-    resolve(f1);
-    });
+// let f1 = 1
+// let f2 = 2
+// let promise = new Promise(function(resolve, reject) {
+//     resolve(f1);
+//     });
     
-    promise.then(
-    result => console.log(f2),
-    reject => console.log("error")
-    );
+//     promise.then(
+//     result => console.log(f2),
+//     reject => console.log("error")
+//     );
     
-    promise.then(console.log)
-    .catch(console.log)//первый вариант
+//     promise.then(console.log)
+//     .catch(console.log)//первый вариант
 
-    promise.then(console.log)//второй вариант
+//     promise.then(console.log)//второй вариант
 
 
+// new Promise(function(resolve, reject) {
+//     setTimeout(() => {
+//       throw new Error("Whoops!");
+//     }, 1000);
+//   }).catch(console.log);
+
+  new Promise(function(resolve, reject) {
+    setTimeout(() => {
+      try {
+        
+       
+  throw new Error("Whoops!");
+      } catch (error) {
+        reject(error);
+      }
+    }, 1000);
+  }).catch(console.log);
