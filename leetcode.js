47e26b053accf2cd942b04300ba3b7c800339025
IrikUnaeturp <137446829+IrikUnaeturp@users.noreply.github.com>
2023-09-16 06:36:10 +0300

    Теорния Promise API и Промисификация

--- leetcode.js
+++ leetcode.js
@@ -24,23 +24,38 @@
 //   }
 // };
 
+//Apply Transform Over Each Element in Array
+let arr =[1,2,3,4]
+let fn = function moreThan10(n) { return n > 10; }
+let filter = function(arr, fn) {
+   let newArr = []
+   for(let i = 0;i<arr.length;i++) {
+    if(fn(arr[i],i)) {
+        newArr.push(arr[i]);
+   }
+   }
+   return newArr;
+};
+  console.log(arr)
+  console.log(filter())
+
 
 // expect(5).toBe(5); // true
 // expect(5).notToBe(5); // "Equal"
  
 //counter2
-let createCounter = function(init) {
-   let  counter =  init
-   return{
-    increment:() => ++counter,
-    decrement:() => --counter,
-    reset:() => counter  = init
-   }
-};
+// let createCounter = function(init) {
+//    let  counter =  init
+//    return{
+//     increment:() => ++counter,
+//     decrement:() => --counter,
+//     reset:() => counter  = init
+//    }
+// };
 
 
-const counter = createCounter(5)
-console.log(counter.increment()); // 6
-console.log(counter.reset()); // 5
-console.log(counter.decrement()); // 4
+// const counter = createCounter(5)
+// console.log(counter.increment()); // 6
+// console.log(counter.reset()); // 5
+// console.log(counter.decrement()); // 4
  
