479b06b52e10f0cac1c3fe5f5e4071af77b4280b
IrikUnaeturp <137446829+IrikUnaeturp@users.noreply.github.com>
2023-09-18 21:56:08 +0300

    ex5

--- filterElements.js
+++
@@ -1,8 +0,0 @@
-// Filter Elements from Array
-let arr = [0,10,20,30]
-let fn = function moreThan10(n) { return n > 10; }
-
-let filter = function (){
-    return arr.filter(fn)
-}
-console.log(filter(arr,fn))
